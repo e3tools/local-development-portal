2cbbcdaade6b8bdb3243b8a8f9663c50cd9ba003
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2022-09-28 10:11:42 +0200

    initial configuration and .gitignore file

--- cosomis/cosomis/urls.py
+++ cosomis/cosomis/urls.py
@@ -12,9 +12,10 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import path
+from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
