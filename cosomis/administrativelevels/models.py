43b9e4f73ac1f3dba6e2893f4f88be9ee21967da
adaboubvincent <adaboubvincent@gmail.com>
2022-10-05 18:43:01 +0200

    Remove the field administrative_level from village and Add the field village to administrativelevel

--- cosomis/administrativelevels/models.py
+++ cosomis/administrativelevels/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class AdministrativeLevel(models.Model):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('AdministrativeLevel', null=True, blank=True, on_delete=models.CASCADE)
+    village = models.ForeignKey('Village', null=True, blank=True, on_delete=models.CASCADE)
     type = models.CharField(max_length=255)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
@@ -17,7 +18,6 @@ class AdministrativeLevel(models.Model):
 class Village(models.Model):
     """Model to present the village"""
     name = models.CharField(max_length=150)
-    administrative_level = models.ForeignKey('AdministrativeLevel', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
