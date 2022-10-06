ae65a5cf657090b7a1304267bb3a2182fafd3aae
adaboubvincent <adaboubvincent@gmail.com>
2022-10-06 19:27:42 +0200

    Updated the url file of administrativelevels app by adding the path to the views of <<<upload CSV file>>>

--- cosomis/administrativelevels/urls.py
+++ cosomis/administrativelevels/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from administrativelevels.views import VillageDetailView
+from administrativelevels.views import UploadCSVView
 
 app_name = 'administrativelevels'
 
 urlpatterns = [
     path('village-detail/<int:pk>/', VillageDetailView.as_view(), name='village_detail'), #The path of the detail of village
+    path('upload/', UploadCSVView.as_view(), name='upload'), #The path to upload CSV file and save in db
 ]
+
+
