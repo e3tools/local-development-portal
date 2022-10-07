836cdfed1ce2ad9bc09a85894c553b79bfd7eff4
adaboubvincent <adaboubvincent@gmail.com>
2022-10-07 13:28:22 +0200

    Updated the function allow to save the administrative level

--- cosomis/administrativelevels/functions.py
+++ cosomis/administrativelevels/functions.py
@@ -32,12 +32,15 @@ def save_csv_file_datas_in_db(datas_file: dict) -> str:
                     elif column == "Village/localité":
                         _type = "Village"
                         parent_type = ("Canton", "Canton")
-                    try:
-                        parent = AdministrativeLevel.objects.get(name=str(datas_file[parent_type[0]][count]).upper(), type=parent_type[1])
-                    except Exception as exc:
-                        parent = None
 
-                    if not AdministrativeLevel.objects.filter(name=name, type=_type, parent=parent):
+                    parent = None
+                    try:
+                        if _type not in ("Region", "Unknow"):
+                            parent = AdministrativeLevel.objects.get(name=str(datas_file[parent_type[0]][count]).upper(), type=parent_type[1]) # Get the parent object of the administrative level
+                    except Exception as exc:
+                        pass
+
+                    if not AdministrativeLevel.objects.filter(name=name, type=_type, parent=parent): # If the administrative level is not already save
                         administrative_level = AdministrativeLevel()
                         administrative_level.name = name
                         administrative_level.type = _type
@@ -49,7 +52,6 @@ def save_csv_file_datas_in_db(datas_file: dict) -> str:
                     
                 except Exception as exc:
                     at_least_one_error = True
-                    print(count)
                     print(exc)
 
             count += 1
