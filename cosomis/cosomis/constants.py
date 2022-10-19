914c4c4274dc89307b5bf4999cd710b9001d5bee
adaboubvincent <adaboubvincent@gmail.com>
2022-10-19 17:10:09 +0200

    Setuped the constants variables of the focus groups for Goal and Obstacle

---
+++ cosomis/cosomis/constants.py
@@ -0,0 +1,12 @@
+
+
+OBSTACLES_FOCUS_GROUP = [
+    "Obstacles of the farmers and breeders group", "Barriers of the women's group",
+    "Barriers for Youth", "Barriers for ethnic minority groups"
+]
+GOALS_FOCUS_GROUP = [
+    "Vision for the focus group of Farmers and breeders", "Vision for the women's focus group",
+    "Vision for the youth focus group", "Vision for the focus group of ethnic minority groups"
+]
+
+
