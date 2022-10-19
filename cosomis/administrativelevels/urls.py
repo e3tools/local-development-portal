38b69ac39eeaf3a1ee93d244fa5f4a67a01f6a5c
adaboubvincent <adaboubvincent@gmail.com>
2022-10-19 17:15:34 +0200

    Setuped the urls path to the class and functions which manage the village development priorities

--- cosomis/administrativelevels/urls.py
+++ cosomis/administrativelevels/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 
-from administrativelevels.views import VillageDetailView
-from administrativelevels.views import AdministrativeLevelsListView
-from administrativelevels.views import UploadCSVView
+from administrativelevels import views
 
 app_name = 'administrativelevels'
 
 urlpatterns = [
-    path('', AdministrativeLevelsListView.as_view(), name='list'), # Administrative levels list path
-    path('village-detail/<int:pk>/', VillageDetailView.as_view(), name='village_detail'), #The path of the detail of village
-    path('upload/', UploadCSVView.as_view(), name='upload'), #The path to upload CSV file and save in db
+    path('', views.AdministrativeLevelsListView.as_view(), name='list'), # Administrative levels list path
+    path('village-detail/<int:pk>/', views.VillageDetailView.as_view(), name='village_detail'), #The path of the detail of village
+    path('upload/', views.UploadCSVView.as_view(), name='upload'), #The path to upload CSV file and save in db
+    path('village/<int:administrative_level_id>/obstacles', views.ObstaclesListView.as_view(), name='priorities_obstacles'),
+    path('village/obstacles/<int:obstacle_id>/delete', views.obstacle_delete, name='obstacle_delete'), #The path to delete obstacle
+    path('village/<int:administrative_level_id>/goals', views.GoalsListView.as_view(), name='priorities_goals'),
+    path('village/goals/<int:goal_id>/delete', views.goal_delete, name='goal_delete'), #The path to delete goal
+    path('village/<int:administrative_level_id>/priorities', views.PrioritiesListView.as_view(), name='priorities_priorities'),
+    path('village/priorities/<int:priority_id>/delete', views.priority_delete, name='priority_delete'), #The path to delete priority
 ]
 
 
