5cf21dbce12a300f93b2ca877872c3bde5dc061b
adaboubvincent <adaboubvincent@gmail.com>
2022-10-21 17:33:54 +0200

    Setuped service function to define the variables overall$

---
+++ cosomis/cosomis/services.py
@@ -0,0 +1,8 @@
+
+
+def overall_variables(request):
+    """Function to define globals variables"""
+    return {
+        'OTHER_LANGUAGES': True, #Variable to define if other languages are setuped
+    }
+
