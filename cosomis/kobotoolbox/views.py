acdd9a3ab00e7a92b57e61732e800e190c50df31
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2022-11-07 15:47:09 +0000

    pulling coordinates from kobo

--- cosomis/kobotoolbox/views.py
+++ cosomis/kobotoolbox/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from django.http import Http404
 
 from kobotoolbox.api_call import get_all
+from administrativelevels.models import AdministrativeLevel
 
 # Create your views here.
 
 
 def kobo(request):
-    print(get_all())
+    all_forms = get_all()
+    print(all_forms)
+    counter = 1
+    for a in all_forms['results']:
+        village = AdministrativeLevel.objects.filter(name=a['Village'])
+        if village:
+            village_object = village.first()
+            coordinates = a['gps'].split(' ')
+            print(counter, village_object.name, a['Village'], coordinates)
+            village_object.latitude = coordinates[0]
+            village_object.longitude = coordinates[1]
+            village_object.save()
+        counter = counter + 1
     raise Http404
     
