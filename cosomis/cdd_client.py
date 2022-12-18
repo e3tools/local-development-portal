bfb15c68295eb4bbde03c8d50284c03d3a079efe
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2022-12-18 15:16:29 +0100

    initial set up of cdd_connector

--- cosomis/cdd_client.py
+++ cosomis/cdd_client.py
@@ -1,14 +1,9 @@
 # This library is meant to be a wrapper to connect
 # To the cdd app couch db database. This depends on the
 # no_sql_client.py library
+import time
 from no_sql_client import NoSQLClient
 from administrativelevels.models import AdministrativeLevel
-
-
-def iterate_administrative_level(adm_list, type):
-
-    for administrative_level in adm_list.filter(type=type):
-        print(administrative_level.name)
 
 
 class CddClient:
@@ -17,32 +12,45 @@ class CddClient:
         self.nsc = NoSQLClient()
         self.adm_db = self.nsc.get_db("administrative_levels")
 
+    def iterate_administrative_level(self, adm_list, type):
+
+        for administrative_level in adm_list.filter(type=type):
+            print("CREATING", administrative_level.name)
+            self.create_administrative_level(administrative_level)
+            print("DONE", administrative_level.name)
+
     def create_administrative_level(self, adm_obj):
         # TODO: You need to manage the parent id from couch.
         # TODO 2: You need to manage the administrative_id.
+        parent = None
+        if adm_obj.parent:
+            parent = str(adm_obj.parent.id)
         data = {
+            "administrative_id": str(adm_obj.id),
             "name": adm_obj.name,
             "administrative_level": adm_obj.type,
             "type": "administrative_level",
-            "parent_id": "",
+            "parent_id": parent,
             "latitude": adm_obj.latitude,
             "longitude": adm_obj.longitude,
         }
-        self.nsc.create_document(self.adm_db, data)
+        nosql_adm = self.nsc.create_document(self.adm_db, data)
+        adm_obj.no_sql_db_id = nosql_adm['_id']
+        adm_obj.save()
         return True
 
     def sync_administrative_levels(self) -> bool:
 
         administrative_levels = AdministrativeLevel.objects.all()
         # Sync Region
-        iterate_administrative_level(administrative_levels, "Region")
+        self.iterate_administrative_level(administrative_levels, "Region")
         # Sync Prefecture
-        iterate_administrative_level(administrative_levels, "Prefecture")
+        self.iterate_administrative_level(administrative_levels, "Prefecture")
         # Sync Commune
-        iterate_administrative_level(administrative_levels, "Commune")
+        self.iterate_administrative_level(administrative_levels, "Commune")
         # Sync Canton
-        iterate_administrative_level(administrative_levels, "Canton")
+        self.iterate_administrative_level(administrative_levels, "Canton")
         # Sync Village
-        iterate_administrative_level(administrative_levels, "Village")
+        self.iterate_administrative_level(administrative_levels, "Village")
 
         return True
