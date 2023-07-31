a14291f4c6769305f9eec4b2769929458dcfba5e
adaboubvincent <adaboubvincent@gmail.com>
2023-07-31 19:10:05 +0000

    Setuped django app assignment with a model for assignment

---
+++ cosomis/assignments/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from assignments.models import AssignAdministrativeLevelToFacilitator
+# Register your models here.
+admin.site.register(AssignAdministrativeLevelToFacilitator)
