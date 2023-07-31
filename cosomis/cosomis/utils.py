45ea2b2c5cb82e5cd44977cb40b0f177edba7fb7
adaboubvincent <adaboubvincent@gmail.com>
2023-07-31 19:12:32 +0000

    Setuped save_facilitator_assignment_in_mis function to record assignments

--- cosomis/cosomis/utils.py
+++ cosomis/cosomis/utils.py
@@ -2,6 +2,7 @@ from subprojects.models import Subproject, Component
 from no_sql_client import NoSQLClient
 from authentication.models import Facilitator
 from administrativelevels.models import AdministrativeLevel
+from assignments.models import AssignAdministrativeLevelToFacilitator
 
 
 def get_facilitators_village_liste(develop_mode=False, training_mode=False, no_sql_db=False, only_ids=True):
@@ -13,16 +14,19 @@ def get_facilitators_village_liste(develop_mode=False, training_mode=False, no_s
         facilitators = Facilitator.objects.using('cdd').filter(develop_mode=develop_mode, training_mode=training_mode)
     for f in facilitators:
         facilitator_db = nsc.get_db(f.no_sql_db_name)
-        docs = facilitator_db.all_docs(include_docs=True)['rows']
+        # docs = facilitator_db.all_docs(include_docs=True)['rows']
+        docs = facilitator_db.get_query_result({"type": 'facilitator'})[:]
         
-        for _doc in docs:
-            doc = _doc.get('doc')
-            if doc.get('type') == 'facilitator':
-                for ad in doc.get('administrative_levels'):
-                    if only_ids:
-                        administrative_levels.append(ad.get('id'))
-                    else:
-                        administrative_levels.append(ad)
+        if docs:
+            # for _doc in docs:
+            # doc = _doc.get('doc')
+            doc = facilitator_db[docs[0]['_id']]
+            # if doc.get('type') == 'facilitator':
+            for ad in doc.get('administrative_levels'):
+                if only_ids:
+                    administrative_levels.append(ad.get('id'))
+                else:
+                    administrative_levels.append(ad)
     return administrative_levels
 
 def attribute_component_to_subprojects(subprojects, component):
@@ -94,3 +98,34 @@ def attribute_project_to_subprojects(subprojects, project):
         subproject.save()
     print()
     print("Done !")
+
+
+
+def save_facilitator_assignment_in_mis(project_id: int, develop_mode=False, training_mode=False, no_sql_db=False):
+    print(">>> Start!")
+    nsc = NoSQLClient()
+    if no_sql_db:
+        facilitators = Facilitator.objects.using('cdd').filter(develop_mode=develop_mode, training_mode=training_mode, no_sql_db_name=no_sql_db)
+    else:
+        facilitators = Facilitator.objects.using('cdd').filter(develop_mode=develop_mode, training_mode=training_mode)
+    for f in facilitators:
+        facilitator_db = nsc.get_db(f.no_sql_db_name)
+        docs = facilitator_db.get_query_result({"type": 'facilitator'})[:]
+        
+        if docs:
+            doc = facilitator_db[docs[0]['_id']]
+            print(doc)
+            for ad in doc.get('administrative_levels'):
+                id_str = ad.get('id')
+                if (id_str and str(id_str).isdigit() and \
+                    not AssignAdministrativeLevelToFacilitator.objects.filter(administrative_level_id=int(id_str), project_id=project_id, activated=True)):
+                    try:
+                        assign = AssignAdministrativeLevelToFacilitator()
+                        assign.administrative_level_id = int(id_str)
+                        assign.facilitator_id = str(f.id)
+                        assign.project_id = project_id
+                        assign.save()
+                    except Exception as exc:
+                        print(exc)
+                        input()
+    print(">>> Done!")
