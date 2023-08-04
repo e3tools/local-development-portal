48964d07ff2f1abc6a0fb8d777b9f580e8513839
adaboubvincent <adaboubvincent@gmail.com>
2023-08-04 13:25:35 +0000

    Setuped tracking modelForms

--- cosomis/subprojects/forms.py
+++ cosomis/subprojects/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Subproject, VulnerableGroup
+from django.utils.translation import gettext_lazy as _
+
+from .models import Subproject, VulnerableGroup, SubprojectStep, Level
 from administrativelevels.models import AdministrativeLevel, CVD
 
 class SubprojectForm(forms.ModelForm):
@@ -35,3 +37,30 @@ class VulnerableGroupForm(forms.ModelForm):
     class Meta:
         model = VulnerableGroup
         fields = '__all__' # specify the fields to be displayed
+
+class SubprojectAddStepForm(forms.ModelForm):
+    begin = forms.DateField(label=_('Begin'), input_formats=['%d/%m/%Y'],
+                                      help_text="DD/MM/YYYY")
+    end = forms.DateField(label=_('End'), input_formats=['%d/%m/%Y'],
+                                      help_text="DD/MM/YYYY", required=False)
+    def __init__(self, *args, **kwargs):
+        # initial = kwargs.get('initial')
+        # doc_id = initial.get('doc_id')
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = SubprojectStep
+        fields = ('step', 'begin', 'end', 'description') # specify the fields to be displayed
+
+
+class SubprojectAddLevelForm(forms.ModelForm):
+    begin = forms.DateField(label=_('Begin'), input_formats=['%d/%m/%Y'],
+                                      help_text="DD/MM/YYYY")
+    end = forms.DateField(label=_('End'), input_formats=['%d/%m/%Y'],
+                                      help_text="DD/MM/YYYY", required=False)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Level
+        fields = ('wording', 'percent', 'ranking', 'begin', 'end', 'description') # specify the fields to be displayed
