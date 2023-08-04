8a95867cb3cd23196b1ff793f616ade90d29be22
adaboubvincent <adaboubvincent@gmail.com>
2023-08-04 13:31:17 +0000

    Modified and added tracking models on Django Admin interface

--- cosomis/subprojects/admin.py
+++ cosomis/subprojects/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Subproject)
-admin.site.register(VulnerableGroup)
-admin.site.register(VillagePriority)
-admin.site.register(VillageMeeting)
-admin.site.register(Component)
-admin.site.register(VillageObstacle)
-admin.site.register(VillageGoal)
-admin.site.register(Financier)
-admin.site.register(Project)
+admin.site.register([
+    Subproject, 
+    VulnerableGroup,
+    VillagePriority,
+    VillageMeeting,
+    Component,
+    VillageObstacle,
+    VillageGoal,
+    Financier,
+    Project,
+    Step
+])
