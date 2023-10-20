310f32898a877ee06333ac07e322863533bf3ff1
NestorBracho <nestorbracho2207@gmail.com>
2023-10-20 14:08:00 +0200

    AdministrativeLevel, Project, Category, Investment, Package, Attachment models and migrations

--- cosomis/administrativelevels/models.py
+++ cosomis/administrativelevels/models.py
@@ -7,22 +7,67 @@ from django.utils.translation import gettext_lazy as _
 from financial.models.bank import Bank
 from cosomis.models_base import BaseModel
 
-
     
 class AdministrativeLevel(BaseModel):
-    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    LIME_GREEN = 'lime_green'
+    DARK_GREEN = 'dark_green'
+    ORANGE = 'orange'
+    RED = 'red'
+    STATUS_COLORS = (
+        (LIME_GREEN, _('Lime Green')),
+        (DARK_GREEN, _('Dark Green')),
+        (ORANGE, _('Orange')),
+        (RED, _('Red'))
+    )
+
+    EARLY = 'early'
+    NORMAL = 'normal'
+    LATE = 'late'
+    BLOCKED = 'blocked'
+    STATUS_DESCRIPTION = (
+        (EARLY, _('Early')),
+        (NORMAL, _('Normal')),
+        (LATE, _('Late')),
+        (BLOCKED, _('Blocked'))
+    )
+
+    VILLAGE = 'village'
+    CANTON = 'canton'
+    COMMUNE = 'commune'
+    REGION = 'region'
+    TYPE = (
+        (VILLAGE, _('Village')),
+        (CANTON, _('Canton')),
+        (COMMUNE, _('Commune')),
+        (REGION, _('Region')),
+    )
     parent = models.ForeignKey('AdministrativeLevel', null=True, blank=True, on_delete=models.CASCADE, verbose_name=_("Parent"))
     geographical_unit = models.ForeignKey('GeographicalUnit', null=True, blank=True, on_delete=models.CASCADE, verbose_name=_("Geographical unit"))
     cvd = models.ForeignKey('CVD', null=True, blank=True, on_delete=models.CASCADE, verbose_name=_("CVD"))
-    type = models.CharField(max_length=255, verbose_name=_("Type"))
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, verbose_name=_("Latitude"))
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, verbose_name=_("Longitude"))
     frontalier = models.BooleanField(default=True, verbose_name=_("Frontalier"))
     rural = models.BooleanField(default=True, verbose_name=_("Rural"))
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, verbose_name=_("Latitude"))
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, verbose_name=_("Longitude"))
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    # adm_0_id to adm_n_id
+    status_color = models.CharField(max_length=20, choices=STATUS_COLORS, default=LIME_GREEN)
+    status_description = models.CharField(max_length=15, choices=STATUS_DESCRIPTION, default=NORMAL)
+    type = models.CharField(max_length=255, verbose_name=_("Type"), choices=TYPE, default=VILLAGE)
+    total_population = models.IntegerField(default=0)
+    population_men = models.IntegerField(default=0)
+    population_women = models.IntegerField(default=0)
+    population_young = models.IntegerField(default=0)
+    population_elder = models.IntegerField(default=0)
+    population_handicap = models.IntegerField(default=0)
+    population_agriculturist = models.IntegerField(default=0)
+    population_pastoralist = models.IntegerField(default=0)
+    population_minorities = models.IntegerField(default=0)
+    main_languages = models.CharField(max_length=50, blank=True, null=True)
+
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     no_sql_db_id = models.CharField(null=True, blank=True, max_length=255)
-
     
     class Meta:
         unique_together = ['name', 'parent', 'type']
@@ -55,7 +100,6 @@ class AdministrativeLevel(BaseModel):
     def get_list_geographical_unit(self):
         """Method to get the list of the all Geographical Unit that the administrative is linked"""
         return self.geographicalunit_set.get_queryset()
-
 
 
 class GeographicalUnit(BaseModel):
@@ -140,7 +184,95 @@ class CVD(BaseModel):
     
     def __str__(self):
         return self.get_name()
-    
+
+
+class Project(BaseModel):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    implementation_agency = models.CharField(max_length=255)
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+
+class Investment(BaseModel):
+    NOT_FUNDED = 'N'
+    IN_PROGRESS = 'P'
+    COMPLETED = 'C'
+    PROJECT_STATUS_CHOICES = (
+        (NOT_FUNDED, _('Not Funded')),
+        (IN_PROGRESS, _('In Progress')),
+        (COMPLETED, _('Completed'))
+    )
+
+    PRIORITY = 'p'
+    SUBPROJECT = 's'
+    INVESTMENT_STATUS_CHOICES = (
+        (PRIORITY, _('Priority')),
+        (SUBPROJECT, _('SubProject'))
+    )
+
+    title = models.CharField(max_length=255)
+    responsible_structure = models.CharField(max_length=255, null=True, blank=True)
+    sector = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='investments')
+    estimated_cost = models.PositiveBigIntegerField()
+    start_date = models.DateField(null=True)
+    duration = models.PositiveIntegerField(help_text=_('In days'))
+    delays_consumed = models.PositiveIntegerField(help_text=_('In days'))
+    physical_execution_rate = models.PositiveIntegerField(help_text=_('Percentage'))
+    financial_implementation_rate = models.PositiveIntegerField(help_text=_('Percentage'))
+    # project_manager_id // TBD Probably is the moderator
+    investment_status = models.CharField(max_length=30, choices=INVESTMENT_STATUS_CHOICES, default=PRIORITY)
+    project_status = models.CharField(max_length=30, choices=PROJECT_STATUS_CHOICES, default=NOT_FUNDED)
+    endorsed_by_youth = models.BooleanField(default=False)
+    endorsed_by_women = models.BooleanField(default=False)
+    endorsed_by_agriculturist = models.BooleanField(default=False)
+    endorsed_by_pastoralist = models.BooleanField(default=False)
+
+
+class Package(BaseModel):
+    PENDING_APPROVAL = 'P'
+    APPROVED = 'A'
+    REJECTED = 'R'
+    UNDER_EXECUTION = 'E'
+    STATUS = (
+        (PENDING_APPROVAL, _('Pending Approval')),
+        (APPROVED, _('Approved')),
+        (REJECTED, _('Rejected')),
+        (UNDER_EXECUTION, _('Under Execution'))
+    )
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='packages')
+    source = models.CharField(
+        max_length=255,
+        help_text=_('Source of the funding, e.g., a particular organization or grant'),
+        blank=True, null=True
+    )
+    funded_investments = models.ManyToManyField(Investment, related_name='packages')
+    draft_status = models.BooleanField(default=True)
+    status = models.CharField(max_length=50, choices=STATUS, default=PENDING_APPROVAL)
+
+
+class Attachment(BaseModel):
+    PHOTO = 'Photo'
+    DOCUMENT = 'Document'
+    TYPE_CHOICES = (
+        (PHOTO, _('Photo')),
+        (DOCUMENT, _('Document'))
+    )
+    adm = models.ForeignKey(AdministrativeLevel, on_delete=models.CASCADE, related_name='attachments')
+    investment = models.ForeignKey(
+        Investment,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='attachments'
+    )
+    url = models.URLField()
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default=DOCUMENT)
+
 
 def update_or_create_amd_couch(sender, instance, **kwargs):
     print("test", instance.id, kwargs['created'])
