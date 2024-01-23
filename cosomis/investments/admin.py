5bf761720ef7990e4091d3a588fffbb8dba59413
NestorBracho <nestorbracho2207@gmail.com>
2024-01-23 13:38:20 +0100

    jvectormap plugin

--- cosomis/investments/admin.py
+++ cosomis/investments/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Investment, Organization
+from .models import Investment, Organization, Sector, Category
 
 
 admin.site.register(Investment)
 admin.site.register(Organization)
+admin.site.register(Sector)
+admin.site.register(Category)
