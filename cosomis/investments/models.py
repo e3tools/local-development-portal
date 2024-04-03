321659875df6cd5280e8213d0703ad8742b23f4c
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2024-04-03 09:50:49 +0200

    small fixes

--- cosomis/investments/models.py
+++ cosomis/investments/models.py
@@ -2,7 +2,6 @@ import os
 from boto3.session import Session
 import requests
 from io import BytesIO
-from PIL import Image
 from django.db import models
 from cosomis.models_base import BaseModel
 from django.utils.translation import gettext_lazy as _
