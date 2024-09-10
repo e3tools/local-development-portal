e0d89277d830b127326605a92d30c874d9032c25
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2024-09-10 13:34:50 +0200

    updates for production server

--- cosomis/administrativelevels/management/commands/sync_subprojects_from_excel.py
+++ cosomis/administrativelevels/management/commands/sync_subprojects_from_excel.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from django.core.management.base import BaseCommand, CommandError
-from administrativelevels.models import Project, AdministrativeLevel
+from administrativelevels.models import Project, AdministrativeLevel, Sector
 from investments.models import Investment
 from thefuzz import fuzz
 
@@ -18,6 +18,7 @@ class Command(BaseCommand):
         project = Project.objects.get(pk=project_id)
         similarity_threshold = 100  # For matching the investment title
         count = 0
+        not_matched = 0
         # Validate that the project exists
         try:
             project = Project.objects.get(pk=project_id)
@@ -76,9 +77,49 @@ class Command(BaseCommand):
                 else:
                     # Handle the case where no similar investment is found
                     # print("No similar investment found for village:", row["village"], index)
-                    pass
+                    not_matched += 1
             else:
                 print("Village not found:", row["village"], index)
+        creating = 0
+        existing_with_village = 0
+        for index, row in df.iterrows():
+            village = AdministrativeLevel.objects.filter(
+                name=row["village"],
+                parent__name=row["parent_adm"],
+                parent__parent__name=row["parent_parent_adm"],
+            )
+            exists = Investment.objects.filter(administrative_level=village.first(), funded_by=project)
+            if not exists:
+                creating += 1
+                sector = Sector.objects.all()
+                other_sector = Sector.objects.get(name="Autre")
+                similarity_threshold = 60
+                matched_sectors = [
+                    sec for sec in sector
+                    if fuzz.ratio(sec.name, row["TYPE D'OUVRAGE (INFRASTRUCTURE)"]) >= similarity_threshold
+                ]
+                try:
+                    cost = float(row["COUT ESTIME DE L'OUVRAGE"])
+                except:
+                    cost = 0
+                Investment.objects.create(
+                    title=row["CDD Option"],
+                    administrative_level=village.first(),
+                    funded_by=project,
+                    longitude=row["Longitude (x)"],
+                    latitude=row["Latitude (y)"],
+                    project_status="F",
+                    sector=matched_sectors[0] if matched_sectors else other_sector,
+                    estimated_cost=cost,
+                    duration=0,
+                    delays_consumed=0,
+                    physical_execution_rate=0,
+                    financial_implementation_rate=0
+                )
+            else:
+                existing_with_village += 1
+                print(exists.first().funded_by)
+
             # Example: Just print the row, you should replace this with your actual processing logic
             #print(row["TYPE D'OUVRAGE (INFRASTRUCTURE)"])
 
@@ -86,4 +127,6 @@ class Command(BaseCommand):
         # project.some_field = some_value_based_on_excel_data
         # project.save()
         print(count)
+        print(f'created amount {creating}')
+        print(f'existing with village {existing_with_village}')
         self.stdout.write(self.style.SUCCESS('Successfully processed the Excel file for project ID "%s"' % project_id))
