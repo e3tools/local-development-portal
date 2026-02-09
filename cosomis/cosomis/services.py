2f6575a88b6fab4179d4e58fbcfa6fc837749baf
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2026-02-09 17:11:45 +0000

    Add MIXPANEL_TOKEN in the global project

--- cosomis/cosomis/services.py
+++ cosomis/cosomis/services.py
@@ -1,8 +1,9 @@
-
+from django.conf import settings
 
 def overall_variables(request):
     """Function to define globals variables"""
     return {
         'OTHER_LANGUAGES': True, #Variable to define if other languages are setuped
+        'MIXPANEL_TOKEN': settings.MIXPANEL_TOKEN
     }
 
