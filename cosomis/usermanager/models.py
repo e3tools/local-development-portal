4ad173c01c8211de5fc305a04f7142170771dada
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2026-04-11 18:24:11 +0000

    Add UserToken model to manage user token API and add password_changed_once attribute to User model to know if the password changed after users automatically register

--- cosomis/usermanager/models.py
+++ cosomis/usermanager/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext_lazy as _
+from datetime import timedelta
+from django.utils import timezone
 
 from cosomis.models_base import BaseModel
 
@@ -101,6 +103,8 @@ class User(AbstractUser):
 
     organization = models.ForeignKey(Organization, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='users')
+    
+    password_changed_once = models.BooleanField(null=True, blank=True, default=None)
 
     objects = UserManager()
 
@@ -124,3 +128,36 @@ class ValidationCode(BaseModel):
     validation_code_ending_datetime = models.DateTimeField(verbose_name=_("Validation Code Ending datetime"))
     motif = models.TextField(verbose_name=_('Motif'))
     already_use = models.BooleanField(default=False, verbose_name=_('Already Use?'))
+
+
+
+# Default token lifetime: 3 years
+TOKEN_LIFETIME = settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] if settings.SIMPLE_JWT and 'ACCESS_TOKEN_LIFETIME' in settings.SIMPLE_JWT else timedelta(days=365*3)
+
+class UserToken(BaseModel):
+    """
+    Stocke les jetons d'accès pour un service API tiers, liés à un utilisateur.
+    """
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='api_token', verbose_name=_('User'))
+    
+    token = models.TextField()
+    
+    refresh_token = models.TextField(
+        null=True, blank=True,
+        default=timezone.now() + TOKEN_LIFETIME,
+        verbose_name="Jeton de Rafraîchissement",
+        help_text="Permet d'obtenir un nouveau jeton d'accès sans reconnexion."
+    )
+    
+    service_name = models.CharField(max_length=100,default='External_API',verbose_name="Nom du Service Tiers")
+    
+    expires_at = models.DateTimeField(default=timezone.now() + TOKEN_LIFETIME)
+
+
+    def __str__(self):
+        return f"Jeton pour {self.user.username} - {self.service_name}"
+
+    def is_valid(self):
+        return self.expires_at > (timezone.now() + timedelta(minutes=5))
+    
